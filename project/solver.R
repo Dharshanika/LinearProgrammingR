fb06df810c49332913681660a376b0e450e8c2b6
vonshick <kubawasik@vp.pl>
2019-11-22 22:38:27 +0100

    Cleaning the structure

--- project/solver.R
+++ project/solver.R
@@ -1,31 +1,30 @@
-# problem: BCC Stores
-
 library(lpSolveAPI)
 library(dplyr)
+library(purrr)
+library(tibble)
 
 create_model <- function(n_variables) {
   return(make.lp(ncol = n_variables))
 }
 
-set_direction <- function(model) {
-    lp.control(model, sense = "max")
+set_direction <- function(model, direction = "max") {
+    lp.control(model, sense = direction)
 }
 
 set_objective <- function(model, inputs, outputs, subject_store_id) {
-  # last element in vector - additional variable u_0 
+  # last element in vector - additional variable u_0
+  # the same element appears in all constraints
   objective_coeffs <- c(as.numeric(outputs[subject_store_id, 2:3]), 1) 
   set.objfn(model, objective_coeffs, indices = c(6, 7, 8))
 }
 
 add_unity_constraint <- function(model, inputs, outputs, subject_store_id) {
-  # last element in vector - additional variable u_0   
   constraint_coeffs <- c(as.numeric(inputs[subject_store_id, 2:6]), 0, 0, 0)
   add.constraint(model, constraint_coeffs, "=", 1)
 }
 
 add_other_constraints <- function(model, inputs, outputs) {
   for(i in 1:nrow(inputs)) {
-    # last element in vector - additional variable u_0 
     constraint_coeffs <- c(-as.numeric(inputs[i, 2:6]),
                            as.numeric(outputs[i, 2:3]), 1)
     add.constraint(model, constraint_coeffs, "<=", 0)
@@ -37,45 +36,34 @@ run_optimization <- function() {
   inputs <- read.csv("inputs.csv", sep = ";", header = TRUE)
   outputs <- read.csv("outputs.csv", sep = ";", header = TRUE)
 
-  stores_number <- nrow(inputs) # k = 19 
-  inputs_number <- ncol(inputs) - 1 # m = 5
-  outputs_number <- ncol(outputs) - 1 # n = 2 
-  combined_changes <- data.frame()
-  combined_efficiency <- data.frame()
+  stores_number <- nrow(inputs) 
+  inputs_number <- ncol(inputs) - 1
+  outputs_number <- ncol(outputs) - 1 
+  proposed_changes <- tibble()
   
   model_size <- inputs_number + outputs_number + 1
-  for(store in 1:stores_number) {
+  
+  efficiency_tibble <- map_df(1:stores_number, function(store) {
     model <- create_model(model_size)
-    set_direction(model)
+    set_direction(model, "max")
     set_objective(model, inputs, outputs, store)
     add_unity_constraint(model, inputs, outputs, store)
     add_other_constraints(model, inputs, outputs)
     solve(model)
     efficiency <- get.objective(model)
 
-    combined_efficiency <- combined_efficiency %>% 
-      bind_rows(
-        data.frame(
-          store = inputs[store, 1], 
-          efficiency = efficiency
-        )
-      )
-
-    print(paste(inputs[store, 1], ": ", efficiency, sep = ""))
+    # sometimes efficiency is extremely close to 1 but not equal to 
+    # probably the reason is calculation error 
+    if (round(efficiency, 5) < 1) {
+      # prepare merged data frame with necessary changes for ineffective
+      # and multiply all columns except name by (1 - efficiency)
+      proposed_changes <<- inputs[store, ] %>% 
+          mutate_at(vars(-X), ~(. * (1 - efficiency))) %>%
+          bind_rows(proposed_changes)
+    }
     
-    if (efficiency < 1) {
-        # print changes
-        for (input in (1:inputs_number) + 1) {
-          print(paste("input", input, "of", inputs[store, 1], "should increase by:", (1 - efficiency) * inputs[store, input], sep = " "))
-        }
-        
-        # prepare merged data frame with necessary changes for ineffective
-        store_changes <- inputs[store, ] # get whole row for specific store
-        store_changes[-1] <- store_changes[-1] * (1 - efficiency) # multiply all columns except name by (1-efficiency)
-        combined_changes <- dplyr::bind_rows(combined_changes, store_changes)
-      }
-  }  
+    return(tibble(store = inputs[store, 1], efficiency = efficiency))
+  })  
   
-  write.csv(combined_changes, "combined_changes.csv")
-  return(list(combined_efficiency, combined_changes))
+  return(list(efficiency_tibble, proposed_changes))
 }
---
+++ sample/solver_cars.R
@@ -0,0 +1,46 @@
+library(lpSolveAPI)
+
+create_model <- function(n_variables) {
+  return(make.lp(ncol = n_variables))
+}
+
+set_direction <- function(model) {
+    lp.control(model, sense = "max")
+}
+
+set_objective <- function(model, inputs, outputs, subject_garage_id) {
+  objective_coeffs <- c(as.numeric(outputs[subject_garage_id, 2:3]))
+  set.objfn(model, objective_coeffs, indices = c(3, 4))
+}
+
+add_unity_constraint <- function(model, inputs, outputs, subject_garage_id) {
+  constraint_coeffs <- c(as.numeric(inputs[subject_garage_id, 2:3]), 0, 0)
+  add.constraint(model, constraint_coeffs, "=", 1)
+}
+
+add_other_constraints <- function(model, inputs, outputs) {
+  for(i in 1:nrow(inputs)) {
+    constraint_coeffs <- c(-as.numeric(inputs[i, 2:3]),
+                           as.numeric(outputs[i, 2:3]))
+    add.constraint(model, constraint_coeffs, "<=", 0)
+  }
+}
+
+run_optimization <- function() {
+
+  inputs <- read.csv("inputs-ex2.csv", sep = ";")
+  outputs <- read.csv("outputs-ex2.csv", sep = ";")
+
+  for(garage in 1:4) {
+    model <- create_model(4)
+    set_direction(model)
+    set_objective(model, inputs, outputs, garage)
+    add_unity_constraint(model, inputs, outputs, garage)
+    add_other_constraints(model, inputs, outputs)
+    solve(model)
+    efficiency <- get.objective(model)
+    print(paste(inputs[garage, 1], ": ", efficiency, sep = ""))
+  }
+
+}
+
